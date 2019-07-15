1812dc53a57a349606fb771ffd7580b574f917cd
Stephen Dios <stephendios@gmail.com>
2019-07-15 13:54:41 -0600

    Completed Actor class. Began Ally class.
    Finished the methods/constructors of Actor class, but cannot test it until Ally class is finished.
    
    Created Ally class but have not filled it out yet.

--- src/entities/Actor.java
+++ src/entities/Actor.java
@@ -8,5 +8,37 @@ public abstract class Actor {
     private String name = "PLACEHOLDER";
     private int initiativeTotal = 0;
 
-    
+    public Actor () {
+
+    }
+
+    public Actor (String name, int initiativeTotal) {
+        setName(name);
+        setInitiativeTotal(initiativeTotal);
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getInitiativeTotal() {
+        return this.initiativeTotal;
+    }
+
+    public void setName(String name) {
+        if(!(name.isEmpty()) && !name.matches(".*\\d.*")) { //if name is not empty AND does not contain any numbers
+            this.name = name;
+        }
+    }
+
+    public void setInitiativeTotal(int initiativeTotal) {
+        if(initiativeTotal >= 0) {
+            this.initiativeTotal = initiativeTotal;
+        }
+    }
+
+    @Override
+    public abstract String toString();
+
 }
---
+++ src/entities/Ally.java
@@ -0,0 +1,5 @@
+package entities;
+
+public class Ally extends Actor{
+
+}
