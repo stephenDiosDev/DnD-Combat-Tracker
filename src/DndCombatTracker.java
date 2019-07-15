5fdb6548688db2c8cedb80cd560897c72d4e8663
Stephen Dios <stephendios@gmail.com>
2019-07-15 13:39:03 -0600

    Added existing repo to this one.

---
+++ src/DndCombatTracker.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class acts as the main launcher for the program
+ */
+public class DndCombatTracker extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
---
+++ src/entities/Actor.java
@@ -0,0 +1,12 @@
+package entities;
+
+/**
+ * Acts as a base class for all other entities. Contains a name and an
+ * initiative total.
+ */
+public abstract class Actor {
+    private String name = "PLACEHOLDER";
+    private int initiativeTotal = 0;
+
+    
+}
