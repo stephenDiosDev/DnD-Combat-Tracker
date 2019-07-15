1cc2cb984d53bbd7e566318c043a7ffe09fc915d
Stephen Dios <stephendios@gmail.com>
2019-07-15 14:19:42 -0600

    Filled out and tested Actor and Ally.

--- src/entities/Ally.java
+++ src/entities/Ally.java
@@ -1,5 +1,30 @@
 package entities;
 
+/**
+ * This class represents the player characters and only contains a name and 
+ * initiative total.
+ */
 public class Ally extends Actor{
 
+    public Ally() {
+        super();
+    }
+
+    public Ally(String name, int initiativeTotal) {
+        super(name, initiativeTotal);
+    }
+
+
+    public String toString() {
+        return "Ally: [Name = " + super.getName() + "] [Initiative Total = " + super.getInitiativeTotal() + "]";
+    }
+
+    public static void main(String[] args) {
+        Ally a = new Ally("Flint ", 14);
+        Ally b = new Ally();
+
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+
+    }
 }
