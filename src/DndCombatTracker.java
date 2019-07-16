0f3b13077e08a8e27fa988e216ac04761005b601
Stephen Dios <stephendios@gmail.com>
2019-07-16 10:36:56 -0600

    Started setup page FXML file.

--- src/DndCombatTracker.java
+++ src/DndCombatTracker.java
@@ -5,8 +5,11 @@ import javafx.stage.Stage;
  * This class acts as the main launcher for the program
  */
 public class DndCombatTracker extends Application {
+
+    public Stage mainStage;
+
     @Override
     public void start(Stage stage) throws Exception {
-        
+        mainStage = stage;
     }
 }
