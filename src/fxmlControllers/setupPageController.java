705ef76d35a0e6bc98f0ce12e108368cd3d49ae1
Stephen Dios <stephendios@gmail.com>
2019-07-16 11:00:31 -0600

    Created setupPage boilerplate controller.

---
+++ src/fxmlControllers/setupPageController.java
@@ -0,0 +1,21 @@
+package fxmlControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class setupPageController {
+
+    @FXML
+    private Button runEncounterBtn;
+
+    @FXML
+    private Button addEnemyBtn;
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private Button addAllyBtn;
+
+}
