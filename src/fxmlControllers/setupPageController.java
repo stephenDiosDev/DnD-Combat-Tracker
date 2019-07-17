a49fd147445de0b2d8998c4af74cf5e534007778
Stephen Dios <stephendios@gmail.com>
2019-07-17 12:40:51 -0600

    Fixed scroll issue with setupPage. TextFields from controller now show up properly.

--- src/fxmlControllers/setupPageController.java
+++ src/fxmlControllers/setupPageController.java
@@ -6,9 +6,29 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.*;
 import java.net.URL;
+import entities.*;
+import java.util.*;
 
 public class setupPageController implements Initializable{
+
+    private int initialAllyAmount = 5;  //this determines, on start up, how many ally/enemy fields to show
+    private int initialEnemyAmount = 5;
+
+    //holds the sorted initiative list of all allies/enemies
+    protected ArrayList<Actor> sortedActorList = new ArrayList<>();
+
+    //holds the (unsorted) list of all ally textfields
+    protected ArrayList<TextField> allyListTextFields = new ArrayList<>(initialAllyAmount);
+
+    //holds the (unsorted) list of all enemy textfields (name and initiative only)
+    protected ArrayList<TextField> enemyListTextFields = new ArrayList<>(initialEnemyAmount);
+
+    //holds the (unsorted) list of all enemy health textfields
+    protected ArrayList<TextField> enemyListHealthTextFields = new ArrayList<>(initialEnemyAmount);
+
+
 
     @FXML
     private Button runEncounterBtn;
@@ -25,6 +45,61 @@ public class setupPageController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("Initialized!");
+
+        for(int i = 0; i < initialAllyAmount; i++) {
+            allyListTextFields.add(new TextField());
+        }
+
+        for(int i = 0; i < initialEnemyAmount; i++) {
+            enemyListTextFields.add(new TextField());
+            enemyListHealthTextFields.add(new TextField());
+        }
+
+        int normalTextFieldPrefWidth = 149;
+        int normalTextFieldPrefHeight = 25;
+
+        int healthTextFieldPrefWidth = 61;
+        int healthTextFieldPrefHeight = normalTextFieldPrefHeight;
+
+        int xCord = 14; //these 3 specify the layout of the textfields
+        int yCord = 14;
+        int yIncrease = 33;
+
+        //Add ally textfields to the main pane
+        for (TextField allyText : allyListTextFields) {
+            allyText.setLayoutX(xCord);
+            allyText.setLayoutY(yCord);
+            allyText.setPrefSize(normalTextFieldPrefWidth, normalTextFieldPrefHeight);
+
+            yCord += yIncrease;
+
+            mainPane.getChildren().add(allyText);
+
+        }
+
+        int xCordMainTextField = 348;   //these 3 are for the specific layout of the enemy name/initiative textfields
+        yCord = 14;
+        yIncrease = 33;
+
+        int xCordHealthTextField = 524; //this is for the specific x layout of the health textfield
+
+        //this loop adds all enemy textfields and health textfields with correct position and layout
+        //and adds them to the main pane in the correct tab order (main textfield -> health textField)
+        for(int i = 0; i < enemyListTextFields.size(); i++) {
+            enemyListTextFields.get(i).setLayoutX(xCordMainTextField);
+            enemyListTextFields.get(i).setLayoutY(yCord);
+            enemyListTextFields.get(i).setPrefSize(normalTextFieldPrefWidth, normalTextFieldPrefHeight);
+
+            enemyListHealthTextFields.get(i).setLayoutX(xCordHealthTextField);
+            enemyListHealthTextFields.get(i).setLayoutY(yCord);
+            enemyListHealthTextFields.get(i).setPrefSize(healthTextFieldPrefWidth, healthTextFieldPrefHeight);
+
+            yCord += yIncrease;
+
+            mainPane.getChildren().add(enemyListTextFields.get(i));
+            mainPane.getChildren().add(enemyListHealthTextFields.get(i));
+        }
+        
     }
 
 }
