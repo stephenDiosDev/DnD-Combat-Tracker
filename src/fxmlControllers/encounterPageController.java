c5dd1a0357fd94234d89a6f4c7fd3ac71866f44d
Stephen Dios <stephendios@gmail.com>
2019-07-19 12:33:34 -0600

    endEncounter button fully implemented.

--- src/fxmlControllers/encounterPageController.java
+++ src/fxmlControllers/encounterPageController.java
@@ -1,5 +1,6 @@
 package fxmlControllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -7,6 +8,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.event.*;
+import javafx.scene.*;
+import javafx.fxml.*;
+import javafx.stage.*;
+import javafx.scene.image.*;
+import application.*;
 
 public class encounterPageController implements Initializable{
 
@@ -25,8 +31,17 @@ public class encounterPageController implements Initializable{
     }
 
     @FXML
-    void endEncounter(ActionEvent event) {
+    void endEncounter(ActionEvent event) throws IOException{
+        //switch FXML page to encounter page
+        Parent root = FXMLLoader.load(getClass().getResource("/fxmlPages/setupPage.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = DndCombatTracker.mainStage;
 
+        stage.setTitle(DndCombatTracker.getStageTitle());
+        stage.getIcons().add(new Image(DndCombatTracker.getWindowIconURL()));
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
