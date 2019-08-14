ebf8e0ffb9d8873e8243f7ecdd76f17a16ca6448
Stephen Dios <stephendiosdeveloper@gmail.com>
2019-08-14 11:44:13 -0600

    Updated version number.

--- src/application/DndCombatTracker.java
+++ src/application/DndCombatTracker.java
@@ -24,7 +24,7 @@ public class DndCombatTracker extends Application {
             blank for full release of minimum viable product
 
     */
-    private static final String stageTitle = "D&D Combat Tracker (v0.8.8a)";
+    private static final String stageTitle = "D&D Combat Tracker (v0.9.0a)";
     private static final String windowIconURL = "/icons/program_icon.png";
 
 
